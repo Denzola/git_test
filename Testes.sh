c8bc460b523029c7e4069ad1016bd6b3a2eddfa4
Denzola <oluwilliamsdenzel+odin@users.noreply.github.com>
2025-06-12 11:33:01 +0100

    Testes.sh

---
+++ Testes.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "You have to try and be calm under pressure remember pressure forms diamonds"
+echo "You have a legacy to keep up and you shall'nt fail"
+echo "Best basketballers have proprioception & you learn slow but when i learn i'm the best"
